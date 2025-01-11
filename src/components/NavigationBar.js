560bda9d0fbc635178e21765a50e95c475739f81
Dennis Clausen <Dennis.Clausen@bbz-rd-eck.de>
2025-01-11 22:25:33 +0100

    4th try

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -17,9 +17,9 @@ function NavigationBar({ buttons, onButtonClick, onNewWindow }) {
   let baseThreshold = 1400;
   // Adjust threshold based on zoom level - adjusting baseThreshold by 10% for each zoom level before calculating zoomAdjustedThreshold
   if (settings.navbarZoom < 1) {
-    baseThreshold -= baseThreshold * (settings.navbarZoom / 6);
+    baseThreshold -= baseThreshold * (settings.navbarZoom / 10);
   } else if (settings.navbarZoom > 1) {
-    baseThreshold += baseThreshold * (settings.navbarZoom / 6);
+    baseThreshold += baseThreshold * (settings.navbarZoom / 7);
   }
   const zoomAdjustedThreshold = baseThreshold * (settings.navbarZoom + (1 / settings.navbarZoom) / 10);
   const showText = windowWidth >= zoomAdjustedThreshold;
